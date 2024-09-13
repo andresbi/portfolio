e56c4bc7142bfb427b92538a8987a6cefafc982f
Andres Scharifker <thedre@Andress-MBP.hsd1.fl.comcast.net>
2024-09-13 07:39:58 -0400

    modularized

--- simple_etl/etl_script.py
+++ simple_etl/etl_script.py
@@ -1,30 +1,57 @@
 import pandas as pd 
 import requests
 
-posts_url = 'https://jsonplaceholder.typicode.com/posts'
-comment_url_template = 'https://jsonplaceholder.typicode.com/posts/{}/comments'
-p_response = requests.get(posts_url)
 
-if p_response.ok:
-    pdf = pd.DataFrame(p_response.json())
+def fetch_data(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an HTTPError for bad responses
+    if response.ok:
+        return response.json()
+    else:
+        print(f"HTTP error occurred: Status code {response.status_code}")
+        return None
+
+def main():
+    #URLs of Interest
+    posts_url = 'https://jsonplaceholder.typicode.com/posts'
+    comment_url_template = 'https://jsonplaceholder.typicode.com/posts/{}/comments'
+
+    p_response = fetch_data(posts_url)
+    post_df = pd.DataFrame(p_response)
 
     # Create an empty dataframe to append to it later
-    cdf = pd.DataFrame()
-    
+    comment_dfs = []
+        
     # Iterate over each post to generate the request.get link
-    for post_id in pdf['id']:
+    for post_id in post_df['id']:
         comment_url = comment_url_template.format(post_id)
-        c_response = requests.get(comment_url)
-     
-    # Obtain the comment list of dictionaries for each post and append to the comment df
-        if c_response.ok:
-            cdf_temp = pd.DataFrame(c_response.json())
-            cdf = pd.concat([cdf,cdf_temp])
-
-else:
-    raise Exception('request not working')
+        c_response = fetch_data(comment_url)
+        if c_response:
+            comment_df = pd.DataFrame(c_response)
+            comment_dfs.append(comment_df)
 
 
-#Validate the data
+        # Obtain the comment list of dictionaries for each post and append to the comment df 
+    if comment_dfs:
+        comment_df = pd.concat(comment_dfs,ignore_index=True)
+        normalize_df(comment_df)
+    else:
+        comment_df = pd.DataFrame()
+    
+    print(comment_df.head(40))
+    print(post_df.head(40))
 
-#Add 2 tables to a sqlite db
+
+def normalize_df(df):
+    if 'email' in df.columns:
+        df['email'] = df['email'].str.lower()
+
+    if 'name' in df.columns:
+        if df['name'].notna().all():
+            print('All Names are present')
+        else:
+            missing_names_count = df['name'].isna().sum()
+            print(f'There are {missing_names_count} missing names in the df')
+
+
+main()
